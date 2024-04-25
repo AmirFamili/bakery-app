175e4811a9f49ff1465cd8841e1e5a9a301b39ad
Amir Famili <44036070+AmirFamili@users.noreply.github.com>
2024-04-25 12:59:06 -0500

    style login

--- src/components/Register/Login.js
+++ src/components/Register/Login.js
@@ -161,7 +161,7 @@ export const Login = () => {
 
 
   return (
-    <section className="bg-gray-main h-screen w-full flex justify-center items-center">
+    <section className="bg-gray-main py-10 w-full flex justify-center items-center">
       <div className=" bg-white border w-2/5  rounded-2xl p-5 relative max-md:w-4/5 pb-20">
         <div className="flex justify-center items-center p-2">
           <img src={logo} alt="بلو کیک" className="w-24 max-md:w-20 " />
